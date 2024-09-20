04c32fb8368ad7177c4fe39a40d736570f65bc8e
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2024-09-20 09:43:11 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,60 @@
+print("Act 9 clases v2")
+print("Jesus Mota Mat: 22308051281275")
+#zona de class
+class Operadores1275:
+    #zona de funciones
+    #suma
+    def suma1275(self,E,N):
+        s1275=E+N
+        print(f"La suma de {E} + {N} = {s1275}")
+        
+    #resta    
+    def resta1275(self,E,N):
+        s1275=E-N
+        print(f"La resta de {E} - {N} = {s1275}")
+        
+    #Multiplicacion
+    def multiplicacion1275(self,E,N):
+        s1275=E*N
+        print(f"La multiplicacion de {E} * {N} = {s1275}")
+        
+    #Division
+    def division1275(self,E,N):
+        s1275=E/N
+        print(f"La division de {E} / {N} = {s1275}")
+        
+    #Modulo
+    def modulo1275(self,E,N):
+        s1275=E%N
+        print(f"El modulo de {E} % {N} = {s1275}")
+        
+    #Exponente
+    def exponente1275(self,E,N):
+        s1275=E**N
+        print(f"El exponente de {E} ** {N} = {s1275}")
+        
+    #Cociente
+    def cociente1275(self,E,N):
+        s1275=E//N
+        print(f"El cociente de {E} // {N} = {s1275}")
+#zona de creacion del objeto
+OperadorMota=Operadores1275()
+
+
+
+#zona de uso de objetos
+print("--Funcion Suma--")
+OperadorMota.suma1275(5,4)
+print("--Funcion Resta--")
+OperadorMota.resta1275(5,4)
+print("--Funcion Multiplicacion--")
+OperadorMota.multiplicacion1275(5,4)
+print("--Funcion Division--")
+OperadorMota.division1275(5,4)
+print("--Funcion Modulo--")
+OperadorMota.modulo1275(5,4)
+print("--Funcion Exponente--")
+OperadorMota.exponente1275(5,4)
+print("--Funcion Cociente--")
+OperadorMota.cociente1275(5,4)
+
